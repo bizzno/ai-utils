0e03fc7776e28528be3e0c8a13594d89e7562249
Alexander Björkén <4242687+bizzno@users.noreply.github.com>
2016-12-21 02:35:37 +0100

    Refactor text_processor.py

---
+++ src/utils/text_processor.py
@@ -0,0 +1,11 @@
+"""Text processing utilities for AI applications"""
+
+def tokenize(text):
+    """Split text into tokens"""
+    return text.lower().split()
+
+def clean_text(text):
+    """Remove special characters and normalize text"""
+    import re
+    return re.sub(r'[^a-zA-Z0-9\s]', '', text)
+Update 7 on 2016-12-21 02:35:37
