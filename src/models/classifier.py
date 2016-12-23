89f9d5e4da3f09c769fc0fae420bd8043cd9e322
Alexander Björkén <4242687+bizzno@users.noreply.github.com>
2016-12-23 15:38:11 +0100

    Fix bug in classifier.py

---
+++ src/models/classifier.py
@@ -0,0 +1,14 @@
+"""Machine learning classifier implementation"""
+
+class TextClassifier:
+    def __init__(self):
+        self.model = None
+
+    def train(self, data, labels):
+        """Train the classifier"""
+        pass
+
+    def predict(self, text):
+        """Make prediction on text"""
+        return 0
+Update 20 on 2016-12-23 15:38:11
